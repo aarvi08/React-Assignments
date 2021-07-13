2dc799da7a7b2f990c067c2888880e01d610b3b7
aarvi08 <shandilyaaarvi@gmail.com>
2021-07-13 16:15:26 +0530

    day 1 assignment

---
+++ src/shivangi.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+
+const Shivangi=()=>{
+  return (
+      <div>
+      <h1 style={{ textAlign: "center"}}>Namaste World</h1>
+      
+      <p>Hey everyone, this is SHIVANGI TRIPATHI , from MORADABAD U.P. I am here to 
+      learn about ReactJS from this amazing platform <b>LetsUpgrade</b> .
+      It is a seven days workshop which gives knowledge from beginners to advanced level,
+      and later by everyone will be awarded by the certification</p>
+
+      <h2><u>THANK YOU LetsUpgrade</u></h2>
+
+      <Button buttonText="one1"/>
+      <Button buttonText="two2"/>
+      <Button buttonText="three3"/>
+
+      </div>
+  )
+}
+
+export default Shivangi
