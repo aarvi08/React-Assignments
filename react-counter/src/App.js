3df52ea9474f2eb4de0125f511405ea06c298c2e
aarvi08 <shandilyaaarvi@gmail.com>
2021-07-14 17:29:24 +0530

    counter app added

---
+++ react-counter/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./style.css"
+
+const App =()=> {
+
+  let [score, setScore]= useState(0)
+
+  return(
+    <div>
+      <h1> This is Shivangi's Counter Application</h1>
+      <h3> The value of score is {score}</h3>
+      <button onClick={()=>{setScore(score+1)}}>Increment</button>
+      <button onClick={()=>(score>0)? setScore(score-1) : ""}>Decrement</button>
+      <button onClick={()=>{setScore(0)}}>Reset</button>
+    </div>
+  )
+}
+
+export default App;
