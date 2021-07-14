f0ec89bc97ecba2b6ccb1904107ff113c6ec9062
aarvi08 <shandilyaaarvi@gmail.com>
2021-07-14 17:47:49 +0530

    increment till 25 , condition added

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -9,7 +9,7 @@ const App =()=> {
     <div>
       <h1> This is Shivangi's Counter Application</h1>
       <h3> The value of score is {score}</h3>
-      <button onClick={()=>{setScore(score+1)}}>Increment</button>
+      <button onClick={()=>(score<25)? setScore(score+1) : "" }>Increment</button>
       <button onClick={()=>(score>0)? setScore(score-1) : ""}>Decrement</button>
       <button onClick={()=>{setScore(0)}}>Reset</button>
     </div>
