81d24cad8294d329c0e7f7cf080c129d4b9063d8
aarvi08 <shandilyaaarvi@gmail.com>
2021-08-09 00:03:35 +0530

    Made the structure of the application using the classes and constructor

--- todo-list/src/to-do.js
+++ todo-list/src/to-do.js
@@ -6,8 +6,34 @@ class TODO extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1>Hello, Welcome to TODO App</h1>
+      <div className="main">
+        <div className="about">
+          <h1>
+            <strong>
+              <em>TODO List App</em>
+            </strong>
+          </h1>
+          <p>
+            This is an application where you can write your list of anything .
+            For e.g. ,
+            <ul>
+              <li>Things you have to do today , tomorrow or in a week.</li>
+              <li>List of items you wanna purchase.</li>
+              <li>List of places you wanna visit etc.</li>
+            </ul>
+            You can make any type of list you want. Welcome here, hoping all the
+            items of your list get done . Have a nice day . It's nice having you
+            with us.
+          </p>
+        </div>
+        <hr />
+        <br />
+
+        <form>
+          <lable>Input the LIST</lable>
+          <input type="text" placeholder="LIST ITEMS" />
+          <button>Add</button>
+        </form>
       </div>
     );
   }
