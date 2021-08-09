db9915570ba3749189542639706ffa938f55e3e2
aarvi08 <shandilyaaarvi@gmail.com>
2021-08-10 00:06:49 +0530

    CSS added . TO_DO list is completed

--- todo-list/src/to-do.js
+++ todo-list/src/to-do.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./index.css";
 class TODO extends React.Component {
   constructor(props) {
     super(props);
@@ -22,50 +22,61 @@ class TODO extends React.Component {
       id: Date.now(),
     };
     this.setState((state) => ({
-      items: state.items.concat(newItem),
+      item: state.item.concat(newItem),
       text: "",
     }));
   }
   render() {
     return (
-      <div className="main">
-        <div className="about">
-          <h1>
-            <strong>
-              <em>TODO List App</em>
-            </strong>
-          </h1>
-          <p>
-            This is an application where you can write your list of anything .
-            For e.g. ,
-            <ul>
-              <li>Things you have to do today , tomorrow or in a week.</li>
-              <li>List of items you wanna purchase.</li>
-              <li>List of places you wanna visit etc.</li>
-            </ul>
-            You can make any type of list you want. Welcome here, hoping all the
-            items of your list get done . Have a nice day . It's nice having you
-            with us.
-          </p>
+      <div>
+        <div className="heading">
+          <strong>
+            <em>
+              <u>TODO List App</u>
+            </em>
+          </strong>
         </div>
-        <hr />
-        <br />
-        <ol>
-          {this.state.items.map((item) => (
-            <li key={item.id}>{item.text}</li>
-          ))}
-        </ol>
-        <form onSubmit={this.handleSubmit}>
-          <lable htmlfor="id">Input the LIST</lable>
-          <input
-            id="id"
-            type="text"
-            placeholder="LIST ITEMS"
-            onChange={this.handleChange}
-            value={this.state.text}
-          />
-          <button>Add</button>
-        </form>
+        <div className="main">
+          <div className="description">
+            <p>
+              <strong>
+                This is an application where you can write your list of anything
+                . For e.g. ,
+                <ul>
+                  <li>Things you have to do today , tomorrow or in a week.</li>
+                  <li>List of items you wanna purchase.</li>
+                  <li>List of places you wanna visit etc.</li>
+                </ul>
+                You can make any type of list you want. Welcome here, hoping all
+                the items of your list get done . Have a nice day . It's nice
+                having you with us.
+              </strong>
+            </p>
+          </div>
+
+          <hr />
+          <br />
+          <ol>
+            {this.state.item.map((item) => (
+              <li key={item.id}>{item.text}</li>
+            ))}
+          </ol>
+          <form onSubmit={this.handleSubmit}>
+            <lable htmlfor="id">
+              <strong>Input the LIST</strong>
+            </lable>{" "}
+            &nbsp;
+            <input
+              id="id"
+              type="text"
+              placeholder="LIST ITEMS"
+              onChange={this.handleChange}
+              value={this.state.text}
+            />{" "}
+            &nbsp;
+            <button>Add</button>
+          </form>
+        </div>
       </div>
     );
   }
